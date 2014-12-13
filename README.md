da21d330250fd60a1ce7240048840e05dbfb6c5e
Albert Lombarte <alombarte@gmail.com>
2014-12-13 17:16:00 +0100

    Added cataloge

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ Quickly configures a machine with the LAMP stack using puppet standalone.
 
 The install script is hardcoded for installing puppet using a CentOS 7 repo.
 
+
+##Usage
+Execute inside your CentOS machine:
+	bash <(curl -s https://raw.githubusercontent.com/sifophp/sifo-provisioning/master/install-puppet-centos.sh)
+
+
 # This is a working in progress project, do not use it yet :)
--- install-puppet-centos.sh
+++ install-puppet-centos.sh
@@ -1,12 +1,12 @@
-yum install -y git
 cd
-git clone --recursive git clone https://github.com/sifophp/sifo-provisioning.git
+yum install -y git
+git clone --recursive https://github.com/sifophp/sifo-provisioning.git
 
 # Install puppet and replace with repo
 rpm -ivh http://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-11.noarch.rpm
 yum install -y puppet
 mv /etc/puppet/ /etc/puppet_original
-ln -s puppet-sifoweb /etc/puppet
+ln -s sifo-provisioning /etc/puppet
 
 # cron task to regularly run puppet apply on a main manifest
 # sudo puppet resource cron puppet-apply ensure=present user=root minute=30 command='/usr/bin/puppet apply /etc/puppet/manifests/site.pp'
