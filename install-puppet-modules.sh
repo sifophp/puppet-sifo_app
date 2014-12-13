f6766e27dfa0952337b9aa2b7211c25409bbc32c
Albert Lombarte <alombarte@gmail.com>
2014-12-13 14:10:38 -0500

    Provisioning using composer

--- install-puppet-modules.sh
+++ install-puppet-modules.sh
@@ -6,3 +6,5 @@ puppet module install puppetlabs-apache --version 1.2
 puppet module install puppetlabs-ntp --version 3.3.0
 puppet module install saz-memcached --version 2.6.0
 puppet module install thias-php --version 1.0.0
+puppet module install puppetlabs-vcsrepo --version 1.2.0
+puppet module install tPl0ch-composer --version 1.3.3
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,13 @@
 # Puppet manifest for an ALL-IN-ONE server (all services in the same machine)
 
+# Useful packages.
+$misc_packages = ['vim-enhanced','telnet','zip','unzip']
+package { $misc_packages: ensure => latest }
+
+# Clock
+include '::ntp'
+
+# Add entries in /etc/hosts:#
 host { 'localhost.localdomain':
     ensure => 'present',
     target => '/etc/hosts',
@@ -12,5 +20,53 @@ class { 'memcached':
     max_memory => 1024,
     max_connections => 2048
 }
+
+# Apache
+class { 'apache':
+	default_mods        => false,
+	default_confd_files => false,
+}
+
+
+# Virtual hosts
+apache::vhost { 'sifo.me':
+    priority        => '0010',
+    port            => '80',
+    docroot         => '/var/www/sifo.me/instances/sifoweb/public/root',
+    # conf_template		=> '/etc/puppet/files/templates/vhost-sifo.conf.erb',
+#    # serveraliases   => ['sifo.me','en.sifo.me', 'ca.sifo.me' ],
+#    # require         => Class['sifo::instances']
+}
+
+# Install composer dependency manager
+include composer
+
+composer::exec { 'sifo-install':
+    cmd                  => 'install',  # REQUIRED
+    cwd                  => '/var/www/sifo.me', # REQUIRED
+    prefer_source        => false,
+    prefer_dist          => false,
+    dry_run              => false, # Just simulate actions
+    custom_installers    => false, # No custom installers
+    scripts              => false, # No script execution
+    interaction          => false, # No interactive questions
+    optimize             => false, # Optimize autoloader
+    dev                  => true, # Install dev dependencies
+    onlyif               => undef, # If true
+    unless               => undef, # If true
+}
+
+# Install the http://sifo.me site:
+vcsrepo { "/var/www/sifo.me":
+    	ensure => present,
+    	provider => git,
+    	source => "https://github.com/sifophp/sifo-app.git",
+    	revision => 'master'
+    }
+
+
+# include php::cli # Installed by composer
+include php::mod_php5
+php::module { [ 'devel', 'pear', 'mysql', 'mbstring', 'xml', 'gd', 'tidy', 'pecl-apc', 'pecl-memcache' ]: }
 
 
