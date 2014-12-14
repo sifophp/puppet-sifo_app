981063173589342895388cb994b25879d75d6cfb
Albert Lombarte <alombarte@gmail.com>
2014-12-14 01:44:40 +0100

    Split services and application

--- manifests/application.pp
+++ manifests/application.pp
@@ -1,37 +1,3 @@
-# Puppet manifest for an ALL-IN-ONE server (all services in the same machine)
-
-# Useful packages.
-$misc_packages = ['vim-enhanced','telnet','zip','unzip']
-package { $misc_packages: ensure => latest }
-
-# Clock
-include '::ntp'
-
-# Add entries in /etc/hosts:#
-host { 'localhost.localdomain':
-    ensure => 'present',
-    target => '/etc/hosts',
-    ip => '127.0.0.1',
-    host_aliases => ['localhost','memcached','mysql']
-}
-
-# Firewall rules in modules/webserverfirewall/manifests/rules.pp:
-include webserverfirewall
-
-
-# Memcached server (128MB)
-class { 'memcached':
-    max_memory => 128,
-    max_connections => 1024
-}
-
-# Apache
-class { 'apache':
-	default_mods        => false,
-	default_confd_files => false,
-}
-
-
 # Virtual hosts
 apache::vhost { 'sifo.me':
     priority        => '0010',
@@ -67,15 +33,3 @@ vcsrepo { "/var/www/sifo.me":
     	source => "https://github.com/sifophp/sifo-app.git",
     	revision => 'master'
     }
-
-
-# include php::cli # Installed by composer
-include php::mod_php5
-php::module { [ 'devel', 'pear', 'mysql', 'mbstring', 'xml', 'gd', 'tidy', 'pecl-apc', 'pecl-memcache' ]: }
-
-php::ini { '/etc/php.ini':
-  display_errors => 'Off',
-  memory_limit   => '128M',
-}
-
-
