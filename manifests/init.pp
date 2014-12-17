da7198f68338af25fb3977e23e3c47b5ff270962
Albert Lombarte <Albert.Lombarte@softonic.com>
2014-12-17 17:17:44 +0100

    Removed Puppetfile from module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,4 @@
 class sifo_app {
-	# Puppet manifest for an ALL-IN-ONE server (all services in the same machine)
-
-	# Useful packages.
-	$misc_packages = ['vim-enhanced','telnet','zip','unzip']
-	package { $misc_packages: ensure => latest }
-
-	# Clock
-	include '::ntp'
+	include 'services.pp'
+	include 'application.pp'
 }
